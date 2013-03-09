579c5c29ca3fb49d2d4d5644adfbdb4c82ae7211
Markus Schirp <mbj@schirp-dso.com>
2013-03-09 17:58:10 +0100

    Use Devtools.init_spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require 'rspec'
-require 'rspec/mocks'
 require 'navigation'
-
-Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each { |f| require(f) }
+require 'devtools'
+Devtools.init_spec_helper
